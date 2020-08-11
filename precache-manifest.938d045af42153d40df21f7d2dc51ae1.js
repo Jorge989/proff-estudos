d94f42341539a0cb0488e79c18cfe0b68b4dd6ab
Jorge Attie <attiejorge89@gmail.com>
2020-08-10 23:13:10 -0300

    Updates

---
+++ precache-manifest.938d045af42153d40df21f7d2dc51ae1.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "728d813aa37854b146238789bd630e2e",
+    "url": "/proff-estudos/index.html"
+  },
+  {
+    "revision": "5d62f259f5524be0231d",
+    "url": "/proff-estudos/static/css/main.6783502f.chunk.css"
+  },
+  {
+    "revision": "f75751bda580555bbec9",
+    "url": "/proff-estudos/static/js/2.26ef5be4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/proff-estudos/static/js/2.26ef5be4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d62f259f5524be0231d",
+    "url": "/proff-estudos/static/js/main.2fbbbe91.chunk.js"
+  },
+  {
+    "revision": "b9bed3a4e573d8eb3d96",
+    "url": "/proff-estudos/static/js/runtime-main.802be0ee.js"
+  },
+  {
+    "revision": "dc8c47fa945838a208074157d997436d",
+    "url": "/proff-estudos/static/media/back.dc8c47fa.svg"
+  },
+  {
+    "revision": "156102ee75a899123483211bbf4a8fbd",
+    "url": "/proff-estudos/static/media/give-classes.156102ee.svg"
+  },
+  {
+    "revision": "211f2bb00860f91804cfb2354e17b255",
+    "url": "/proff-estudos/static/media/landing.211f2bb0.svg"
+  },
+  {
+    "revision": "86fef799b0726c66fb7a9d710e2d922c",
+    "url": "/proff-estudos/static/media/logo.86fef799.svg"
+  },
+  {
+    "revision": "bfdb3ebe513b5b2411a5014d6aad3abe",
+    "url": "/proff-estudos/static/media/purple-heart.bfdb3ebe.svg"
+  },
+  {
+    "revision": "e7a7e1b4e016245fa97e6f42aee4bcaa",
+    "url": "/proff-estudos/static/media/study.e7a7e1b4.svg"
+  },
+  {
+    "revision": "6dfaa2f94fc54e53666a511b779f90b4",
+    "url": "/proff-estudos/static/media/warning.6dfaa2f9.svg"
+  },
+  {
+    "revision": "45c6e6ecebde5ee441644d41cc132ce4",
+    "url": "/proff-estudos/static/media/whatsapp.45c6e6ec.svg"
+  }
+]);
